715ba32712ab9b8ff0a475e1b7026b762d19f290
Fiore1717 <biffioravante@gmail.com>
2024-12-20 15:10:10 +0100

    Passo 1

--- corsaSpensierata2/src/App.java
+++ corsaSpensierata2/src/App.java
@@ -1,20 +1,25 @@
+// PASSO 1: GENERAZIONE PERCORSO
 import java.util.Random;
 import java.util.Scanner;
-import java.util.ArrayList;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
         Random r = new Random();
-
         System.out.println("La gara ha inizio!");
+        System.out.println();
+        String[] percorso = { "strada", "buca", "muro", "piscina" };
         
-        String [] percorso = {"strada", "buca","muro", "piscina"};
-        String [] azioni = {"corri","salta","arrampicati","nuota"};    
-        int numeroCorridori = 5;
-
-        ArrayList <String> azioniCorridori =  new ArrayList<>(numeroCorridori);
-
-
+        System.out.println("Di quanti elementi deve essere composto il percorso?");
+        int lunghezzaPercorso = sc.nextInt();        
+       
+        System.out.println("Percorso generato:");
+        // Genero il percorso in maniera casuale in base all'input dell'utente
+            for (int i = 0; i <lunghezzaPercorso; i++) {
+                String ostacolo = percorso[r.nextInt(percorso.length)];
+                System.out.print(ostacolo + " ");                
+            }
+        sc.close();         
     }
 }
